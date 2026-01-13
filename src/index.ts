b28256a198fa3272a4ad9ecb5840addef0e55d5e
Abiria <oro3673@gmail.com>
2026-01-13 10:02:13 -0500

    refactor env config loading at index

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,43 @@ import { GatewayIntentBits } from 'discord.js'
 import { Logger } from 'tslog'
 import { Storage } from './db'
 import { Bot, type BotConfig } from './bot'
+import { env } from 'node:process'
 
-const log = new Logger({ name: 'main' })
+const LOG_LEVEL = env.LOG_LEVEL ? parseInt(env.LOG_LEVEL, 10) : undefined
 
-const TOKEN = process.env.DISCORD_TOKEN
-const CLIENT_ID = process.env.CLIENT_ID
-const GUILD_ID = process.env.GUILD_ID
+const logger = new Logger({
+	minLevel: LOG_LEVEL,
+})
 
-if (!TOKEN || !CLIENT_ID) {
-	log.fatal('Missing environment variables. Please check README.')
+if (env['DISCORD_TOKEN'] === undefined) {
+	logger.fatal(
+		'Missing environment variable DISCORD_TOKEN. Please check README.',
+	)
 	process.exit(1)
 }
+
+const DISCORD_TOKEN = env['DISCORD_TOKEN']
+
+if (env['CLIENT_ID'] === undefined) {
+	logger.fatal('Missing environment variable CLIENT_ID. Please check README.')
+	process.exit(1)
+}
+const CLIENT_ID = env['CLIENT_ID']
+
+if (env['GUILD_ID'] === undefined) {
+	logger.fatal('Missing environment variable GUILD_ID. Please check README.')
+	process.exit(1)
+}
+const GUILD_ID = env['GUILD_ID']
 
 const DB_PATH = process.env.DB_PATH || 'vc_logger.db'
 
 // Initialize DB
-const storage = new Storage(DB_PATH, log)
+const storage = new Storage(DB_PATH, logger)
 
 // Init Bot
 const botConfig: BotConfig = {
-	token: TOKEN,
+	token: DISCORD_TOKEN,
 	clientId: CLIENT_ID,
 	guildId: GUILD_ID,
 }
@@ -31,7 +48,7 @@ const bot = new Bot(
 		intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 	},
 	botConfig,
-	log,
+	logger,
 	storage,
 )
 
